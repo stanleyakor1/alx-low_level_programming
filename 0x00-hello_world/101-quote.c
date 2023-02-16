33d37985039a97c06a0ac365e115deb070f8fb1a
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-16 01:56:48 -0800

    First commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,8 @@
  * Return: Always 0 (Success)
  */
 #include<stdio.h>
-int main(void) {
-    fprintf(stderr, "\"And that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+int main(void)
+{
+    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
     return (1);
 }
