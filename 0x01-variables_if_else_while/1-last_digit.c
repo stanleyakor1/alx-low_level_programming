2ed64e31cbd5af9f45a21e2c4dec334a8fa03faf
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-17 02:03:41 -0800

    First commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 	else if (last > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 
-	else if(last < 6 && last != 0)
+	else if (last < 6 && last != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 
 	return (0);
