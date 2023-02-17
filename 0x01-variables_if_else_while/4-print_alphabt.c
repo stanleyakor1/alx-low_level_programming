260161f82eb3707dc8a246cc28c80127ee03dcd3
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-17 04:05:01 -0800

    First commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,16 @@
 int main(void)
 {
 	char letter;
+
 	letter = 'a';
 	while (letter <= 'z')
 	{
-	if ((letter != 'e') && ( letter != 'q'))
+	if (letter != 'e' &&  letter != 'q')
 	{
 		putchar(letter);
 	}
 	letter++;
 	}
 	putchar('\n');
-        return (0);
+	return (0);
 }
