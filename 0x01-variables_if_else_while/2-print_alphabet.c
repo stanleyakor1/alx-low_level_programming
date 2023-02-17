dd9031ce9a987bcad77211afc0f788eff3016647
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-16 23:38:06 -0800

    First commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char letter;
+	letter ='a';
+	while(letter<='z')
+	{
+	putchar(letter);
+	letter++;
+	}
+	putchar('\n');
+
+        return (0);
+}
