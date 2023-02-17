084a19cc5e6f6bb5154d2cbac17df90b1c530570
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-17 04:50:56 -0800

    First commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,25 +8,25 @@
  * Return: Always 0 (Success)
  */
 int main(void) {
-  int x, y,z;
-  for (x = 0; x < 10; x++)
-  {
-    for (y = x + 1; y < 10; y++)
-    {
-	 for(z=y+1;z<10;z++)
+	int x, y, z;
+	
+	for (x = 0; x < 10; x++)
 	{
-      putchar('0' + x);
-      putchar('0' + y);
-      putchar('0'+z);
-      if(x*y*z<504)
-      {
-    	  putchar(',');
-     	  putchar(' ');
+		for (y = x + 1; y < 10; y++)
+		{
+			for(z = y + 1; z < 10; z++)
+			{
+				putchar('0' + x);
+				putchar('0' + y);
+				putchar('0' + z);
+				if (x * y * z < 504)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
-
-	}
-    }
-  }
-  putchar('\n');
-  return (0);
+	putchar('\n');
+	return (0);
 }
