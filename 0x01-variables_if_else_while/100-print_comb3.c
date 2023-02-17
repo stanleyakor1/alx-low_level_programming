82c81217e78472f29c5666e1fb424f72fe02f197
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-17 04:41:43 -0800

    First commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,20 +9,20 @@
  */
 int main(void)
 {
-  int x, y;
-  for (x = 0; x < 10; x++)
-  {
-    for (y = x + 1; y < 10; y++)
-    {
-      putchar('0' + x);
-      putchar('0' + y);
-      if(x*y<72)
-      {
-    	  putchar(',');
-     	  putchar(' ');
+	int x, y;
+  	for (x = 0; x < 10; x++)
+  	{
+		for (y = x + 1; y < 10; y++)
+		{
+			putchar('0' + x);
+			putchar('0' + y);
+			if( x*y <72)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
-    }
-  }
-  putchar('\n');
-  return (0);
+	putchar('\n');
+	return (0);
 }
