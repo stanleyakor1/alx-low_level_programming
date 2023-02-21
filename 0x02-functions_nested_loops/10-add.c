5469a251a39e6a05313395bfd77a3d5d5062c466
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-21 09:18:50 -0800

    First commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * add - adds two integers
+ * @a: first integer.
+ * @b: second integer.
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
