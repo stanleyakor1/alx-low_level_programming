63b99dd50c5586a8dd17cbceb34ec4dd3b684b3d
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-21 04:16:27 -0800

    First commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return: Always 0.
  */
-void print_alphabet()
+void print_alphabet(void)
 {
 	char alpha = 'a';
 
