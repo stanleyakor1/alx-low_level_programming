1bdd27b3029283d52aa5b5365787b44192883c90
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-21 04:10:01 -0800

    First commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * main - Prints _putchar as a message.
+ * print_alphabet_x10 - Prints _putchar as a message.
  * Description: It prints the characters _putchar.
- * print_alphabet_x10 - 
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
