ab9624c1dead6405befc2738085a9d4d3ac81f27
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-21 14:38:40 -0800

    First commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,40 +8,44 @@ void times_table(void)
 {
 	int x,y,z;
 	for (x = 0; x < 10; x++)
-	{
+	{	
+		_putchar('0' + 0);
 		for (y = 0; y < 10; y++)
 		{
 			z = x * y;
-			if (z == 0)
+			_putchar(',');
+			_putchar(' ');
+			if (z / 10 == 0)
 			{
-				if (y == 0)
+				_putchar(' ');
+				_putchar('0' + z);
+			/*	if (y == 0)
 					_putchar('0');
 				else
 				{
 					_putchar(' ');
 					_putchar('0');
-				}
+				} */
 			}
 
-			else if (z > 0 && z / 10 <= 0)
+		/*	else if (z > 0 && z / 10 <= 0)
 			{
 				_putchar(' '); 
 				_putchar('0' + z);
-			}
+			}*/
 			else
 			{
 				_putchar('0' + z / 10);
 				_putchar('0' + z % 10);
 			}
 	
-			if ( y <= 8)
+/*			if ( y <= 8)
 			{
 				_putchar(',');
 			}
-			_putchar(' ');
+			_putchar(' '); */
 		}
 		_putchar('\n');
 
 	}
 }
-
