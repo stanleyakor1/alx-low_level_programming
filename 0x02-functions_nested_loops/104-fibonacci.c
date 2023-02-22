78121482f6539514a28c590007fa79f575b28da6
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-21 18:35:05 -0800

    F

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,32 +7,46 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	long unsigned int x = 1, y = 2, init;
-	int i = 0;
+	unsigned long x1 = 1;
+	unsigned long x2 = 1;
+	unsigned long x3 = 0;
+	long t1, t2, h1, h2, x3_h, x3_t;
+	int i = 2, bust;
 
-	printf("%lu", x);
-	printf(", "); 
-	printf("%lu", y);
-	printf(", ");
-	while (i < 99 )
+	printf("1");
+
+	while (i < 93)
 	{
-		init = x + y;
-		if (i < 98)
-		{
-			printf("%lu", init); 
-			printf(", ");
-		}
-		else
-		{
-			printf("%lu", init);
-		}
-		
-		x = y;
-		y = init;
+		x3 = x1 + x2;
 		i++;
+		printf(", %lu", x3);
+		x1 = x2;
+		x2 = x3;
 	}
+
+	h1 = x1 / 1000000000;
+	t1 = x1 % 1000000000;
+	h2 = x2 / 1000000000;
+	t2 = x2 % 1000000000;
+
+	while (i < 99)
+	{
+		bust = (t1 + t2) / 1000000000;
+		x3_t = (t1 + t2) - (1000000000 * bust);
+		x3_h = (h1 + h2) + bust;
+		i++;
+		printf(", %lu%09lu", x3_h, x3_t);
+
+		h1 = h2;
+		t1 = t2;
+		h2 = x3_h;
+		t2 = x3_t;
+	}
+
+
 	printf("\n");
+
 	return (0);
 }
