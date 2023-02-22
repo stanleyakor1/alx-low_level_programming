a135f2769301155fe3acaeda139226690a23ac14
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-21 18:31:22 -0800

    F

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,18 @@
-int _putchar(char a)
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	char a[] = '_putchar';
+	char a[8] = "_putchar";
 	int n = 0;
 
 	do {
-		
-
+		_putchar(a[n]);
+		n++;
+	} while (n < 8);
+	_putchar('\n');
+	return (0);
 }
