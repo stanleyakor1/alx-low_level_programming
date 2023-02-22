374210de1155c8333224a7747efacda32e022e2a
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-21 17:07:50 -0800

    Improved

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,47 @@
 #include "main.h"
 /**
- * main - check the code
- *
+ * print_times_table - check the code
+ *@n: size
  * Return: Always 0.
  */
 void print_times_table(int n)
-{
+{	
 	int x, y, z, mid;
 
-	if( n < 0 || n > 15)
+	if (n < 0 || n > 15)
 	{
 	}
 	else
 	{
 		for (x = 0; x <= n; x++)
 		{
-			for (y = 0; y <= n; y++)
+			_putchar('0' + 0);
+			for (y = 1; y <= n; y++)
 			{
 				z = x * y;
-				if (z < 10)
+				_putchar(',');
+				_putchar(' ');
+				if (z / 10 == 0)
 				{
 					_putchar(' ');
 					_putchar(' ');
 					_putchar('0' + z);
 				}
-				else if (z >= 10 && z < 100)
+				else if (z / 10 > 0 && z / 10 < 10)
 				{
 					_putchar(' ');
 					_putchar('0' + z / 10);
 					_putchar('0' + z % 10);
 				}
-				else 
+				else
 				{
 					mid = z / 10;
 					_putchar('0' + z / 100);
 					_putchar('0' + mid % 10);
 					_putchar('0' + z % 10);
 				}
-				if ( y <= n-1)
-				{
-					_putchar(',');
-				}
-				_putchar(' ');
 			}
 			_putchar('\n');
-
 		}
 	}
 }
-
