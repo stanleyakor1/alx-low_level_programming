03f1ec2150f3e3e2833d24168a4c9e13f89d5d2f
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-23 14:57:22 -0800

    First commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
