17a0e7f939f8b7751b21731e34f4436866b01d24
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-23 15:19:11 -0800

    First commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * print_most_numbers - print numbers 0-9 except 2 & 4
+ *
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+       	{
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + i);
+		}
+		i++;
+	} 
+	_putchar('\n');
+}
