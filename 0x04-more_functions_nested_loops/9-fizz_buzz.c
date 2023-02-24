18ec5ade23369127f9a79aca2e8fe4df1c66c36e
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-23 18:19:05 -0800

    F

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i = 1;
 
-	printf("%d",i);
+	printf("%d", i);
 	for (i = 2; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
@@ -17,13 +17,10 @@ int main(void)
 		else if (i % 5 == 0 && i % 3 != 0)
 			printf(" Buzz");
 		else if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf(" FizzBuzz");
-		}
 		else
-		{
 			printf(" %d", i);
-		}
+		
 	}
 	printf("\n");
 	return (0);
