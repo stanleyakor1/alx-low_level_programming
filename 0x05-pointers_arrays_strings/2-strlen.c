5b7d1760053c69a9f303c15284e4d2ea5af701c0
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-26 21:30:43 -0800

    F

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/* betty style doc for function main goes there */
+/**
+ * _strlen - returns length of the string
+ * @s: string
+ *
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	return strlen(s);
+}
