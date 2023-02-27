4fe77985941fa6fd0f39b86d5fff706de4cfb0e4
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-26 21:10:06 -0800

    First commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * reset_to_98 - change n to 98
+ * @n: integer
+ *
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n  = 98;
+}
