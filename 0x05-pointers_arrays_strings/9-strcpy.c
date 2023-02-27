656a4111de1a611880e3bd83dc685f27b98804b1
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-27 00:38:43 -0800

    P

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/* betty style doc for function main goes there */
+/**
+ * *_strcpy - copies a string
+ * @src: source
+ * @dest: destination
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int n = strlen(src);
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
