1c862f93c173b0b0349e817011bae4c82d955720
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-26 21:46:09 -0800

    P

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/* betty style doc for function main goes there */
+/**
+ * _putschar - returns length of the string
+ * @str: string
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
