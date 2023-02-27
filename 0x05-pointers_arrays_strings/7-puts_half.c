2cda71c5d0768cf88bd1c034dbb553a8f341d9b7
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-26 23:17:04 -0800

    P

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/* betty style doc for function main goes there */
+/**
+ * puts_half - prints half of a string
+ * @s: string
+ *
+ * Return: Always 0 (Success)
+ */
+void puts_half(char *s)
+{
+	int n = strlen(s);
+	int i = n / 2;
+
+	while (i < n)
+	{
+		putchar(s[i]);
+		i++;
+	}
+	putchar('\n');
+}
