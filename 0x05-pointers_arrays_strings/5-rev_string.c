5bed30c7ae03b83852fc984ee01bb5ffc39ec7e7
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-02-26 22:42:00 -0800

    P

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,21 +3,21 @@
 #include <string.h>
 /* betty style doc for function main goes there */
 /**
- * _strlen - returns length of the string
+ * _rev_string - reverses a string
  * @s: string
  *
  * Return: Always 0 (Success)
  */
 void rev_string(char *s)
-{	
+{
 	int i;
 	int len = strlen(s);
 
 	for (i = 0; i < len / 2; i++)
 	{
 		char temp = s[i];
+
 		s[i] = s[len - i - 1];
 		s[len - i - 1] = temp;
 	}
-	
 }
