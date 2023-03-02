bb93389d3f5b65f4be277ccf065079ac3ec4b708
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-02 00:15:44 -0800

    Pass

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,26 +17,3 @@ int _strcmp(char *s1, char *s2)
 	}
 	return (*s1 - *s2);
 }
-/*
-int _strcmp(char *s1, char *s2)
-{
-	int count = 0;
-	int i = 0;
-	int l1 = strlen(s1);
-	int l2 = strlen(s2);
-
-	while (*s1 && *s2)
-	{
-		if (s1[i] == s2[i])
-			count++;
-		s1++;
-		s2++;
-		i++;
-	}
-	if (count == l1 && count == l2)
-		return (0);
-	else if (s1[0] > s2[0])
-		return (15);
-	else
-		return (-15);
-}*/
