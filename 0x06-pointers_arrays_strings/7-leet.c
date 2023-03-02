87ce0fae224f74a9ed8538a3f6df901255b96186
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-02 03:05:34 -0800

    Pass

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,21 +10,19 @@
 char *leet(char *ptr)
 {	
 	char *a = ptr;
-	char leet_chars[] = "AEGOTL";
-	char leet_nums[] = "436017";
+	char leet_chars[] = "AaEeGgOoTtLl";
+	char leet_nums[] = "443366007711";
 	int i;
 
 	while (*a)
 	{
-		i = 0;
-		while (leet_chars[i])
+		for (i = 0; i < 12; i++)
 		{
-			if (*a == leet_chars[i] || *a == leet_chars[i] + 32)
+			if (*a == leet_chars[i])
 			{
 				*a = leet_nums[i];
 				break;
 			}
-			i++;
 		}
 		a++;
 	}
