872e7696571c012f8abfe6983181485cf0890b62
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-02 15:41:38 -0800

    Convert string to num

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,31 +1,24 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
-/* betty style doc for function main goes there */
 /**
- * leet - converts string to num
- * @ptr: pointer
- * Return: Always 0 (Success)
+ *leet- encode a string into 1337
+ *@s: string pointer
+ *Return: s
  */
-char *leet(char *ptr)
-{	
-	char *a = ptr;
-	char leet_chars[] = "aAeEoOtTlL";
-	char leet_nums[] = "4433007711";
-	int i;
+char *leet(char *s)
+{
+	int i, j;
 
-	while (*a)
+	char src[] = "aeotlAEOTL";
+	char dest[] = "4307143071";
+
+
+	for (i = 0; *(s + i); i++)
 	{
-		i = 0;
-		do {
-			if (*a == leet_chars[i])
-			{
-				*a = leet_nums[i];
-				break;
-			}
-			i++;
-		} while (i < 12);
-		a++;
+		for (j = 0; j < 10; j++)
+		{
+			if (src[j] == *(s + i))
+				*(s + i) = dest[j];
+		}
 	}
-	return (ptr);
+	return (s);
 }
