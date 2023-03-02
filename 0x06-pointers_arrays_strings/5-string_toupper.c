92a7db8ced832e44505fbbc3e02be87031b621a2
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-01 20:23:30 -0800

    Pass

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,7 @@
 #include <string.h>
 /* betty style doc for function main goes there */
 /**
- * string_toupper - reverses an array
+ * string_toupper- reverses an array
  * @a: pointer
  * Return: Always 0 (Success)
  */
