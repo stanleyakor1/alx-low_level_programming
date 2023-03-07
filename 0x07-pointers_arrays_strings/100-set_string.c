5f8d4f5f2a6f6c773cb38d700e3a105614482202
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-06 17:21:21 -0800

    first

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * set_string -  prints elements of a 2d array
+ * @s : address to a pointer
+ * @to: new pointer
+ *Return: pointer
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
