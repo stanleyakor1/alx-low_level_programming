97c5f550fad8773e0831ed77e2b8c254208864f7
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-08 02:27:00 -0800

    First commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * is_prime_number-  checks if number is prime.
+ * @n: number
+ * Return: prints if prime, 0 otherwise.
+ */
+int more(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (more(n, i - 1));
+}
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (more(n, n - 1));
+}
