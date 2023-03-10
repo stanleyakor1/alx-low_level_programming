e9db3dad709f20db1fe485933d5757d51a98231c
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-10 03:08:03 -0800

    Fixed

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,49 +8,33 @@
  * @argv: array of input arguments
  * Return: number of coin
  */
+
 int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
-	
-	if (argc != 2)
+	int cents, ncoins = 0;
+
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-       	cents = atoi(argv[1]);
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
+
+	cents = atoi(argv[1]);
+
 	while (cents > 0)
 	{
 		if (cents >= 25)
-		{
 			cents -= 25;
-			coins++;
-		}
 		else if (cents >= 10)
-		{
 			cents -= 10;
-			coins++;
- 		}
 		else if (cents >= 5)
- 		{
 			cents -= 5;
-			coins++;
-		}
 		else if (cents >= 2)
- 		{
- 			cents -= 2;
- 			coins++;
-		}
-		else
-		{
- 			cents -= 1;
- 			coins++;
- 		}
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
 	}
-	printf("%d\n", coins);
+	printf("%d\n", ncoins);
 	return (0);
 }
