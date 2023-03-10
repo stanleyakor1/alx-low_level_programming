d3eeb1134b5e29b99094dacfb41533f6d8c6d521
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-10 00:14:13 -0800

    Fixed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,14 +7,8 @@
  * @argv: array of input arguments
  * Return: prints all the input arguments
  */
-int main(int argc, char** argv)
+int main(int argc __attribute__((unused)), char** argv)
 {
-	int i = 0;
-
-	do 
-	{
-		printf("%s\n",argv[i]);
-		i++;
-	} while (i < argc);
+	printf("%s\n",argv[0]);
 	return (0);
 }
