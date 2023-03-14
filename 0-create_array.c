658bb81ac5f662774e32aba6cbdb01a22dfda8bf
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-13 23:49:42 -0700

    First commit

---
+++ 0-create_array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * create_array- creates an array and intializes it with special characters.
+ * @size: the array size
+ * @c: the pointer to the string
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *vec = (char *) malloc(sizeof(char) * size);
+	unsigned int i = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	if (vec == NULL)
+	{
+		return (NULL);
+	}
+	
+	while (i < size)
+	{
+			vec[i] = c;
+			i++;
+	}
+	return (vec);
+}
