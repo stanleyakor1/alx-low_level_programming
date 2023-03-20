8b64e45f9cbd4d27dfdbb7e41a3a878ad00d6177
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-20 11:59:53 -0700

    FIXED

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,11 +5,11 @@
  *@name: name of dog
  *@age: age of dog
  *@owner: owner of dog
- *Return: void
+ *Return: no return
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d -> name = name;
-	d -> age = age;
-	d -> owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
