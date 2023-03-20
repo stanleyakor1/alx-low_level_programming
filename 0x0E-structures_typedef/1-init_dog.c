2d821a87b2bf4c7a2390dca4e55fd1017894ceb7
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-20 02:16:41 -0700

    Passed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,14 +1,15 @@
 #include "dog.h"
 /**
  *init_dog - Initialize the attributes of dog
- *@d - pointer to struct dog
- *@name - name of dog
- *@age- age of dog
- *@owner - owner of dog
+ *@d:pointer to struct dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ *Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-        d -> name = name;
-        d -> age = age;
-        d -> owner = owner;
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
 }
