46ab6f4c5ecc1166f3813111e99d21fa4d061db4
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-20 17:36:51 -0700

    Print name of file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* betty style doc for function main goes there */
+/**
+ * main- function main
+ * @argc: number of input arguments
+ * @argv: array of input arguments
+ * Return: void
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
