29124557fb893d016a84e20010af2e5eb09e50fa
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-22 14:57:15 -0700

    search for integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,15 +10,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp)
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+	i = 0;
+	while (i < size)
 	{
-		if (size <= 0)
-			return (-1);
-
-		for (i = 0; i < size; i++)
-			if (cmp(array[i]))
-				return (i);
+		if (cmp(array[i]))
+			return (i);
+		i++;
 	}
-
 	return (-1);
 }
