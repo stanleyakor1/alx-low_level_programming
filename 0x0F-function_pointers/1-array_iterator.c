f68811b72f380cc865d54ad9e70852a71c792648
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-22 14:51:56 -0700

    array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (array && action)
-		do {
+		for (i = 0; i < size; i++)
 			action(array[i]);
-			i++;
-		} while (i < size);
 }
+
