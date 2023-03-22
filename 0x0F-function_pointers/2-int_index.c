8d8b8b93c3a002f6330dbae19ce394f3dd17a106
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-22 14:31:58 -0700

    Compare array values

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ *int_index - compare two numbers
+ *@array : values to be compared
+ *@size : array size
+ *@cmp: compare functon
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+	if (size <= 0)
+		return (-1);
+	if (array && cmp)
+	{
+		do {
+			if(cmp(array[i]))
+				return (i);
+			i++;
+		} while (i < size);
+	}
+	return (-1);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef _FUNCTION_POINTERS_
 #define _FUNCTION_POINTERS_
 #include <stdlib.h>
+int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 int _putchar(char);
 void array_iterator(int *array, size_t size, void (*action)(int));
