792047df92982deba3308bba7c0977b1740fdbfc
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-22 14:15:55 -0700

    Print an array

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,7 @@
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
+#include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
 int _putchar(char);
 void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
