47d22fc96dfd7c6e401e6802ad0019f3e711018f
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-22 14:33:30 -0700

    Compare array values

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,12 +9,13 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
+
 	if (size <= 0)
 		return (-1);
 	if (array && cmp)
 	{
 		do {
-			if(cmp(array[i]))
+			if (cmp(array[i]))
 				return (i);
 			i++;
 		} while (i < size);
