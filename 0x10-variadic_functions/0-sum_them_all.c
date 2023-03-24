1c9f6da737132a947a1aec6c7fb8af3ce40e6998
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-23 17:09:34 -0700

    Variadic sum fixed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,10 +9,10 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int total = 0, i = 0;
 	va_list ap;
-	va_start(ap, n);
 
 	if (n == 0)
 		return (0);
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
 		total += va_arg(ap, int);
 	va_end(ap);
