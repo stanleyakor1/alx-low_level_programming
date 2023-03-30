4a0c89bfea28f54349e35449d8e7667bbdb5ea8e
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-30 16:13:42 -0700

    print singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,10 +10,10 @@ size_t print_list(const list_t *h)
 	size_t count;
 
 	count = 0;
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
+		if (!h->str)
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
