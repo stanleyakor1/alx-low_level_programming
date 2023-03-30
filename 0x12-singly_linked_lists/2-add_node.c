a9ab1d26bfdc8c7042323b833bcfb4883779d131
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-30 16:58:16 -0700

    add nodes to a singly linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,38 +1,29 @@
 #include "lists.h"
 /**
- *list_t - add to a singly linked list
- *@head : pointer to the list
- *str : string pointer
- *Return: The new list
- *
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newNode;
-	char *str_copy;
-	size_t length;
+	list_t *new;
+	size_t nchar;
 
-	length = 0;
-	if (!head || !str)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-	newNode = malloc(sizeof(list_t));
-	if (!newNode)
-		return (NULL);
-	str_copy = strdup(str);
-	if (!str_copy)
-	{
-		free(newNode);
-		return (NULL);
-	}
-	while (*str)
-	{
-		length++;
-		str++;
-	}
-	newNode->str = str_copy;
-	newNode->len = length;
-	newNode->next = *head;
-	*head = newNode;
+
+	new->str = strdup(str);
+
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+
+	new->len = nchar;
+	new->next = *head;
+	*head = new;
 
 	return (*head);
 }
