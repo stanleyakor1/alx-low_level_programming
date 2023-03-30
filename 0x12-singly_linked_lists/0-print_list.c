84a7a6f851f6c34f1fad032948fd316ef4624de4
stanleyakor1 <stanleyakor@u.boisestate.edu>
2023-03-30 15:58:42 -0700

    print singly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *print_list - prints the elements  of a list
+ *@h : pointer to the structure
+ *Return: The number of nodes
+ *
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
